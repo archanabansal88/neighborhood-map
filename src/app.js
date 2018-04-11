2b673e283ac03d029095b98a59b85050afc7482c
Archana Bansal <archanamittal0388@gmail.com>
2018-04-11 10:45:43 +0530

    Feat: Filter search locations

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import ko from 'knockout'
 import Map from './map'
 import ViewModel from './viewModel'
+import location from './locations'
 
 function initMap () {
   const map = new Map()
-  ko.applyBindings(new ViewModel())
+  ko.applyBindings(new ViewModel(location))
 }
 
 window.initMap = initMap
--- src/viewModel.js
+++ src/viewModel.js
@@ -1,13 +1,24 @@
 import ko from 'knockout'
 
 export default class ViewModel {
-  constructor () {
+  constructor (location) {
+    this.location = location
     this.isSideBarVisible = ko.observable(false)
+    this.searchedText = ko.observable('')
+    this.filterLocation = ko.computed(() => {
+      return this.location.filter((detail) => detail.title.toLowerCase().indexOf(this.searchedText().toLowerCase()) !== -1)
+    })
   }
+
   showSideBar () {
     this.isSideBarVisible(true)
   }
+
   hideSideBar () {
     this.isSideBarVisible(false)
+  }
+
+  displayVenue (location) {
+    console.log(location)
   }
 }
