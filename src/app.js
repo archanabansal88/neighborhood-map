c4421f1746afc2c6f5336d2e13a02846cd4f9535
Archana Bansal <archanamittal0388@gmail.com>
2018-04-13 23:15:08 +0530

    Feat: comments added

--- src/app.js
+++ src/app.js
@@ -4,12 +4,18 @@ import ViewModel from './viewModel'
 import location from './locations'
 import Venue from './venue'
 
+/**
+ * @description bootstrap the application when google map is loaded
+ */
 const initMap = () => {
   const venueDetails = location.map((venue) => new Venue(venue))
   const map = new Map(venueDetails)
   ko.applyBindings(new ViewModel(venueDetails, map, true))
 }
 
+/**
+ * @description handles error when google map fails to load
+ */
 const handleMapError = () => {
   ko.applyBindings(new ViewModel([], {}, false))
 }
