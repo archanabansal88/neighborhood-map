56eb44c9d6d61f3885e502a6ff85838ad2f6ff09
Archana Bansal <archanamittal0388@gmail.com>
2018-04-13 21:53:23 +0530

    Feat: Added support for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
+const webpack = require('webpack')
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
   entry: './src/app.js',
   output: {
     path: __dirname,
-    filename: './build/main.js'
+    filename: './dist/main.js'
   },
   resolve: {
     extensions: ['.js']
@@ -11,5 +14,40 @@ module.exports = {
     inline: true,
     port: 3000,
     historyApiFallback: true
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015', 'stage-2']
+          }
+        }
+      },
+      {
+        test: /\.(png)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'url-loader'
+        }
+      }
+    ]
+  },
+  plugins: isProd ? [
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        screw_ie8: true,
+        warnings: false
+      },
+      comments: false
+    }),
+    new webpack.optimize.AggressiveMergingPlugin()
+  ] : []
 }
